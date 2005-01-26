f7101c19dc38a7597db5b036cf8213639ff38e9b
Eric Bréchemier <eric@bezen.org>
2005-01-26 15:01:38 +0100

    Replace MathFP with Beartronics fplib for Fixed Point math values
    I had to add the function stringToFP() to handle the conversion
    from a string value to a fixed point value. This function is
    present in MathFP, as int toFP(String s) but lacking in fplib,
    which is initialized from a regular integer instead.
    
    The current implementation of stringToFP() does not handle
    a negative floating point value correctly: the sign is only
    applied to the integer part, while the fractional part is
    always parsed as a positive value, which results in a
    discrepancy in the sum to form the full Fixed Point value.
    Thus:
    
      int fpValue = fpIPart + fpFPart;
      return fpValue;
    
    should be rewritten:
    
      int fpValue = fpIPart < 0 ? fpIPart - fpFPart: fpIPart + fpFPart;
    
    Also, multiplication by -1 can be done directly on a Fixed Point
    integer, which means that:
    
      FP.Mul(, -FP.ONE )
      FP.Mul(fpSinAngle, -FP.ONE )
    
    can be simply written as:
    
      -fpCosAngle
      -fpSinAngle

--- src/encoder/java/name/brechemier/eric/JavaEncoder.java
+++ src/encoder/java/name/brechemier/eric/JavaEncoder.java
@@ -4,20 +4,21 @@ import java.io.FileOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-import net.jscience.math.kvm.MathFP; // for float/double conversions to int
+import com.beartronics.FP; // for float/double conversions to int
 
 /**
  * Used by XSLT transforms to create a binary document with values
  * easy to decode in a Java application (or binary for java file). <br />
  * 
  * As there are no floating points on the target mobile phone device,
- * we encode floats as equivalent integers using the MathFP library. <br />
+ * we encode floats as equivalent integers using Beartronics Fixed Point Library. <br />
  * 
  * Version 1.0: created on July 20th, 2004    <br />
  * Version 1.1: created on October 26th, 2004 <br />
+ * Version 1.2: created on January 19th, 2005 <br />
  *
  * @author Eric Bréchemier
- * @version 1.1
+ * @version 1.2
  */
 public class JavaEncoder
 {
@@ -40,9 +41,7 @@ public class JavaEncoder
    
    // 360° = 2PI (rad)
    //   1° = PI/180 (rad)
-   protected static final int FP_DEG_TO_RAD_FACTOR = MathFP.div(MathFP.PI, MathFP.toFP(180));
-   
-   protected static final int FP_MINUS_ONE = MathFP.toFP("-1");
+   protected static final int FP_DEG_TO_RAD_FACTOR = FP.Div(FP.PI, FP.intToFP(180));
    
    protected DataOutputStream output;
    
@@ -111,59 +110,97 @@ public class JavaEncoder
       _singleton.output.writeLong(data);
    }
    
+   protected static int stringToFP(String floatNumber)
+   {
+     int comaPos = floatNumber.indexOf('.');
+     
+     int iPart;
+     int fPartAsInt = 0;
+     int fPartCoeff = 1;
+     
+     if (comaPos!=-1) 
+     {
+       iPart = Integer.parseInt( floatNumber.substring(0,comaPos) );
+       
+         //      comaPos=5  lastPos=9
+         // 01234.6789 floatPartLength=4
+       int lastPos = floatNumber.length()-1;
+       int floatPartLength = lastPos - comaPos;
+       
+       if (floatPartLength > 0) 
+       {
+         fPartAsInt = Integer.parseInt( floatNumber.substring(comaPos+1) );
+         
+         for (int i=0; i<floatPartLength; i++) {
+           fPartCoeff *= 10;
+         }
+       }
+     }
+     else
+     {
+       iPart = Integer.parseInt( floatNumber );
+     }
+     
+     int fpIPart = FP.intToFP(iPart);
+     int fpFPart = FP.intToFP(fPartAsInt)/fPartCoeff;
+     
+     int fpValue = fpIPart + fpFPart;
+     return fpValue;
+   }
+   
    protected static int fpConvertAngleDegToRad( int fpDegAngle )
    {
-      return MathFP.mul(FP_DEG_TO_RAD_FACTOR,fpDegAngle);
+      return FP.Mul(FP_DEG_TO_RAD_FACTOR,fpDegAngle);
    }
    
    public static void writeRadAngleString(String floatDegAngle) throws IOException
    {
-      int fpRadAngle = fpConvertAngleDegToRad( MathFP.toFP(floatDegAngle) );
+      int fpRadAngle = fpConvertAngleDegToRad( stringToFP(floatDegAngle) );
       _singleton.output.writeInt( fpRadAngle );
    }
    
    public static void writeCosAngleString(String floatDegAngle) throws IOException
    {
-      int fpRadAngle = fpConvertAngleDegToRad( MathFP.toFP(floatDegAngle) );
-      _singleton.output.writeInt( MathFP.cos(fpRadAngle) );
+      int fpRadAngle = fpConvertAngleDegToRad( stringToFP(floatDegAngle) );
+      _singleton.output.writeInt( FP.Cos(fpRadAngle) );
    }
    
    public static void writeSinAngleString(String floatDegAngle) throws IOException
    {
-      int fpRadAngle = fpConvertAngleDegToRad( MathFP.toFP(floatDegAngle) );
-      _singleton.output.writeInt( MathFP.sin(fpRadAngle) );
+      int fpRadAngle = fpConvertAngleDegToRad( stringToFP(floatDegAngle) );
+      _singleton.output.writeInt( FP.Sin(fpRadAngle) );
    }
    
    public static void writeMinusCosAngleString(String floatDegAngle) throws IOException
    {
-      int fpRadAngle = fpConvertAngleDegToRad( MathFP.toFP(floatDegAngle) );
-      int fpCosAngle = MathFP.cos(fpRadAngle);
-      _singleton.output.writeInt( MathFP.mul(fpCosAngle, FP_MINUS_ONE )  );
+      int fpRadAngle = fpConvertAngleDegToRad( stringToFP(floatDegAngle) );
+      int fpCosAngle = FP.Cos(fpRadAngle);
+      _singleton.output.writeInt( FP.Mul(fpCosAngle, -FP.ONE )  );
    }
    
    public static void writeMinusSinAngleString(String floatDegAngle) throws IOException
    {
-      int fpRadAngle = fpConvertAngleDegToRad( MathFP.toFP(floatDegAngle) );
-      int fpSinAngle = MathFP.sin(fpRadAngle);
-      _singleton.output.writeInt( MathFP.mul(fpSinAngle, FP_MINUS_ONE )  );
+      int fpRadAngle = fpConvertAngleDegToRad( stringToFP(floatDegAngle) );
+      int fpSinAngle = FP.Sin(fpRadAngle);
+      _singleton.output.writeInt( FP.Mul(fpSinAngle, -FP.ONE )  );
    }
    
    
    
    public static void writeFloatString(String floatData) throws IOException
    {
-      _singleton.output.writeInt( MathFP.toFP(floatData) );
+      _singleton.output.writeInt( stringToFP(floatData) );
    }
    
    public static void writeFloat(float data) throws IOException
    {
-      // encoding with MathFP
+      // encoding with FPLib
       writeFloatString( Float.toString(data) );
    }
    
    public static void writeDouble(double data) throws IOException
    {
-      // encoding with MathFP
+      // encoding with FPLib
       writeFloatString( Double.toString(data) );
    }
    
